9b80d724572b29509c95d6a1ba945885fe69a292
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2022-03-07 13:40:39 +0000

    Update MobileViewAlert.tsx

--- src/components/MobileViewAlert.tsx
+++ src/components/MobileViewAlert.tsx
@@ -1,6 +1,6 @@
 export default function MobileViewAlert() {
   return (
-    <div className="flex flex-col h-screen bg-[url('/background.jpg')] bg-cover">
+    <div className="flex flex-col h-screen bg-[url('/img/splash.jpg')] bg-cover splash-image">
       <div className="flex flex-col my-auto mx-auto items-center p-6 text-center text-white font-bold">
         Please open this website in your desktop browser, mobile is currently
         unsupported.
