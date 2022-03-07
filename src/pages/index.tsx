d8c41b7c58d30c593e9b025b5e901d86f4bc15b8
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2022-03-07 17:37:33 +0000

    Add lore to claim page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,42 @@ export default function Index() {
     <div>
       <Layout>
         <div className="max-w-7xl mx-auto py-12 sm:px-6 lg:px-12 bg-white">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 font-proximanovabold">
-            Claim
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 font-proximanovabold text-center">
+            Mystery Egg Airdrop
           </h1>
-          <div className="mb-4 mt-4 text-xl text-gray-700 dark:bg-gray-700 dark:text-gray-300 font-proximanovaregular">
+          <div className="mb-6 mt-12 text-md text-gray-700 dark:bg-gray-700 dark:text-gray-300 font-proximanovaregular text-center">
             <p className="font-medium">
-              Enter our whitelist raffle for your chance to win one of our
-              limited edition NFTs.
+              It was a hot and dry day on Planet Mellow, water was scarce and
+              the heat was creating its first victims in the northern side of
+              the village.
+            </p>
+            <br />
+            <p className="font-medium">
+              It had been only a few weeks since the peaceful Chicks were forced
+              to walk through the portal out of Planet Chicco. Everyone was
+              really worried and wondering if they could ever go back to their
+              beloved and happy planet they still call home.
+            </p>
+            <br />
+            <p className="font-medium">
+              One night, two brothers were on their way back home but a powerful
+              light caught their attention. It was the most vibrant light they’d
+              ever seen and it drew them in its direction similar to how
+              mermaids have a magnetic pull on sailors.
+            </p>
+            <br />
+            <p className="font-medium">
+              As they got closer to the light the intensity got stronger and
+              stronger to the point where they could barely see. But after a
+              while they started to see a shape and realized that before them
+              there was an Egg. A magic egg? Why was it irradiating such a
+              powerful light and how? That was a mystery...
+            </p>
+            <br />
+            <p className="font-medium">
+              Then they remembered an ancient legend the Elders always liked
+              telling, saying that only a lucky person could get his hands on
+              the Mystery Egg.. yes but who is this person?
             </p>
           </div>
           <div className="pt-6 w-full flex justify-center">
@@ -29,7 +58,7 @@ export default function Index() {
           </div>
           {nftWhitelist && (
             <div>
-              <div className="pt-10 w-full flex justify-center font-proximanovaregular text-lg">
+              <div className="pt-10 w-full flex justify-center font-proximanovaregular text-lg font-medium">
                 Congratulations, your address is eligible for our whitelist
                 raffle!
               </div>
