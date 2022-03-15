23c5db1c575b879bd9cb50a9e7773c9b0db15e7a
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2022-03-15 07:11:24 +0000

    Update URL_BACKEND_BASE

--- src/utils/solchickConsts.ts
+++ src/utils/solchickConsts.ts
@@ -3,7 +3,7 @@ export const SOLCHICK_NFT_EXCHANGE_PROGRAM_ID = process.env
 export const SOLCHICK_DECIMALS_ON_SOL = 9;
 export const NFT_EXCHANGE_PROGRAM_IDL = require(`@/idl/nft_exchange.json`);
 
-const URL_BACKEND_BASE = process.env.REACT_APP_BACKEND_URL as string;
+const URL_BACKEND_BASE = process.env.NEXT_PUBLIC_BACKEND_URL as string;
 
 export const URL_SERVER_INFO = () => `${URL_BACKEND_BASE}/api/status`;
 
